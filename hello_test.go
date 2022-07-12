cd1444f12df078de42338c76392b854edfdaa494
josephpanossian <josephpanossian@gmail.com>
2022-07-12 15:57:12 -0700

    multi language hello

--- hello_test.go
+++ hello_test.go
@@ -11,15 +11,35 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Joseph")
+		got := Hello("Joseph", "")
 		want := "Hello, Joseph"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello in French", func(t *testing.T) {
+		got := Hello("George", "French")
+		want := "Bonjour, George"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say hello in Tagalog", func(t *testing.T) {
+		got := Hello("Luciano", "Tagalog")
+		want := "Kamusta, Luciano"
 
 		assertCorrectMessage(t, got, want)
 	})
